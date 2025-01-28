3ffc5acf7e93fd939fc602df05d186c637204010
Stefano Esposito <stefano.esposito97@outlook.com>
2025-01-28 18:22:30 +0100

    fixed setup.py for volsurfs

--- setup.py
+++ setup.py
@@ -11,15 +11,16 @@ setup(
     install_requires=[
         "numpy>=1.24.4",
         "tqdm>=4.66.1",
-        "torch>=2.3.1",
+        "torch>=2.1.0",
         "pillow>=9.4.0",
-        "torchvision>=0.18.1",
+        "torchvision>=0.16.0",
         "iopath>=0.1.10",
-        "matplotlib>=3.9.1",
+        "matplotlib",
         "jupyter>=1.0.0",
         "opencv-python>=4.7.0",
         "open3d>=0.18.0",
         "rich>=13.9.4",
         "pycolmap==0.4.0"
     ],
+    python_requires=">=3.8",
 )
